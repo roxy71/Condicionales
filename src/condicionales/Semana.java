517ca7f1e95cddaba7b7d65e12649a2978f44af6
roxy71 <roxana71janet@gmail.com>
2017-11-03 20:46:45 -0300

    Se registran los dias de la semana

--- src/condicionales/Semana.java
+++ src/condicionales/Semana.java
@@ -4,11 +4,41 @@
  * and open the template in the editor.
  */
 package condicionales;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author LAB03
  */
-public class Semana {
-    
+public class Semana 
+{
+         public static void main(String[] args)
+         {
+              int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));
+              switch(numero)
+              
+              {
+                  case 1:
+                  JOptionPane.showMessageDialog(null, "Lunes");
+                  break;
+                  case 2:
+                  JOptionPane.showMessageDialog(null, "Martes");
+                  break;
+                  case 3:
+                  JOptionPane.showMessageDialog(null, "Miercoles");
+                  break;
+                  case 4:
+                   JOptionPane.showMessageDialog(null, "Jueves");
+                  break;
+                  case 5:
+                   JOptionPane.showMessageDialog(null, "Viernes");
+                  break;
+                  case 6:
+                   JOptionPane.showMessageDialog(null, "Sabado");
+                  break;
+                  case 7:
+                   JOptionPane.showMessageDialog(null, "Domingo");
+                  break;
+              } 
+              
+         }
 }
