5241525bf1b45cdf2eda5628f9774acf450d17ca
roxy71 <roxana71janet@gmail.com>
2017-11-02 21:07:25 -0300

    Aca se calculas las notas,si es mayor a 4 APRUEBA, si es menor a 4 REPRUEBA

--- src/condicionales/Calificacion.java
+++ src/condicionales/Calificacion.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package condicionales;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author LAB03
  */
-public class Calificacion {
-    
-}
+public class Calificacion 
+{
+    public static void main(String[] args)
+    {
+        
+            float nota1, nota2, nota3, promedio;
+        
+         nota1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el nota1"));
+         nota2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el nota2"));
+         nota3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el nota3"));
+         
+         promedio=(nota1 + nota2 + nota3) /3;
+         
+         if (promedio>=4)//Aqui va una condicion
+         
+         {
+          //Si la condicion se cumple se ejecutara este codigo
+          JOptionPane.showMessageDialog(null, "Aprobo");
+         }
+         else//Si no se cumple la condicion se ejecuta esto
+         {
+          JOptionPane.showMessageDialog(null, "Reprobo");
+           
+          }
+              
+          }
+         }
+
